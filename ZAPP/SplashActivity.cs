899cadaf0d36b75fc6f45fe5a3d5d5e7c65dcd5e
Laptop Duane <duanejaspers@gmail.com>
2020-08-27 10:15:04 +0200

    added splash screen

---
+++ ZAPP/SplashActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Android.Content;
+using Android.Views;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace ZAPP
+{
+    using System.Threading;
+    using Android.App;
+    using Android.OS;
+
+    [Activity(Theme = "@style/Theme.Splash", MainLauncher = true,
+                                             NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            _database db = new _database(this);
+            //db.login();
+
+            Thread.Sleep(10000);
+
+            StartActivity(typeof(Home));
+        }
+
+    }
+}
